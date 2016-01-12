894c17009a238e33d2208dc791c1dadb29115142
Simon Harte <simon.harte@namics.com>
2016-01-12 13:55:15 +0100

    clean up and fix tests

--- test/main.js
+++ test/main.js
@@ -15,14 +15,11 @@ const chalk = require('chalk');
 const runTests = function(t, opt) {
 	const suffix = opt.targetPre;
 
-	opt.sassShouldCompile = opt.sassShouldCompile === undefined ? true : opt.sassShouldCompile;
-	opt.jsonShouldCompile = opt.jsonShouldCompile === undefined ? true : opt.jsonShouldCompile;
-
 	let fileObj;
 	let failedCompilation = false;
 	let failedJsonCompilation = false;
 
-	const stream = through2.obj((file, encoding, done) => {
+	const stream = through2.obj(function(file, encoding, done) {
 		if(file.path.match('stub')) {
 			fileObj = file;
 		}
@@ -38,7 +35,7 @@ const runTests = function(t, opt) {
 			failedJsonCompilation = true;
 			t.end();
 		})
-		.pipe(through2.obj((file, encoding, done) => {
+		.pipe(through2.obj(function(file, encoding, done) {
 			if(!file.path.match('stub')) {
 				t.equal(file.contents.toString().split('\n').length, 6, 'test json should result in a 6 line file');
 			} else {
@@ -50,15 +47,9 @@ const runTests = function(t, opt) {
 		}))
 		.pipe(concat('test.' + suffix))
 		.pipe(suffix === 'less' ? less() : sass())
-		.on('end', function() {
-			if(opt.jsonShouldCompile && !failedJsonCompilation) {
-				t.pass('json compiled successfully, as expected');
-			} else if(!opt.jsonShouldCompile && failedJsonCompilation) {
-				t.pass('json failed to compile, as expected');
-			} else if(opt.jsonShouldCompile && failedJsonCompilation) {
-				t.fail('json failed to compile when expected to pass');
-			} else if(!opt.jsonShouldCompile && !failedJsonCompilation) {
-				t.fail('json compiled when it was expected to fail');
+		.on('end', () => {
+			if(failedJsonCompilation) {
+				t.fail('json failed to compile');
 			}
 		})
 		.on('error', () => {
@@ -80,14 +71,14 @@ const runTests = function(t, opt) {
 	}));
 
 	stream.end();
-}
+};
 
-function setupTest(name, opt) {
+const setupTest = function(name, opt) {
 	test(name, (t) => {
-		console.log(chalk.yellow('Test: ' + name) + chalk.green(' (' + opt.targetPre + ' mode)'));
+		console.log(chalk.cyan('Test: ' + name) + chalk.green(' (' + opt.targetPre + ' mode)'));
 		runTests(t, opt);
 	})
-}
+};
 
 const preprocessor = ['scss', 'sass', 'less'];
 
